86f5c34271da08be191c64c60b1827e7faa51ce5
Karandeep Singh <karandeep070104@gmail.com>
2025-09-24 01:05:05 +0530

    Docker image build and ecr repository

--- networksecurity/cloud/s3_sync.py
+++ networksecurity/cloud/s3_sync.py
@@ -1,10 +1,18 @@
-import os
+import subprocess
 
 class S3Sync:
     def sync_folder_to_s3(self, folder, aws_bucket_url):
-        command = f"aws s3 sync {folder} {aws_bucket_url}"
-        os.system(command)
-    
+        command = ["aws", "s3", "sync", folder, aws_bucket_url]
+        result = subprocess.run(command, capture_output=True, text=True)
+        if result.returncode != 0:
+            raise RuntimeError(f"Failed to sync to S3: {result.stderr}")
+        else:
+            print(result.stdout)
+
     def sync_folder_from_s3(self, folder, aws_bucket_url):
-        command = f"aws s3 sync {aws_bucket_url} {folder}"
-        os.system(command)
+        command = ["aws", "s3", "sync", aws_bucket_url, folder]
+        result = subprocess.run(command, capture_output=True, text=True)
+        if result.returncode != 0:
+            raise RuntimeError(f"Failed to sync from S3: {result.stderr}")
+        else:
+            print(result.stdout)
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -28,16 +28,16 @@ from networksecurity.constants.training_pipeline import (
     TRAINING_BUCKET_NAME,
     SAVED_MODEL_DIR
 )
-from networksecurity.cloud import S3Sync
+from networksecurity.cloud.s3_sync import S3Sync
 
 class TrainingPipeline:
     def __init__(self):
-        self.trainig_pipeline_config=TrainingPipelineConfig()
-        self.s3_model=S3Sync()
+        self.training_pipeline_config=TrainingPipelineConfig()
+        self.s3_sync=S3Sync()
         
     def start_data_ingestion(self):
         try:
-            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.trainig_pipeline_config)
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Start Data Ingestion")
             data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
@@ -48,7 +48,7 @@ class TrainingPipeline:
     
     def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact):
         try:
-            data_validation_config=DataValidationConfig(training_pipeline_config=self.trainig_pipeline_config)
+            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
             data_validation=DataValidation(data_ingestion_artifact, 
                                            data_validation_config=data_validation_config)
             logging.info("Inititate data validation")
@@ -60,7 +60,7 @@ class TrainingPipeline:
         
     def start_data_transformation(self, data_validation_artifact: DataValidationArtifact):
         try:
-            data_transformation_config=DataTransformationConfig(training_pipeline_config=self.trainig_pipeline_config)
+            data_transformation_config=DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
             data_transformation=DataTransformation(data_validation_artifact,
                                                    data_transformation_config,
                                                    )
@@ -73,7 +73,7 @@ class TrainingPipeline:
     def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact):
         try:
             model_trainer_config=ModelTrainerConfig(
-                training_pipeline_config=self.trainig_pipeline_config
+                training_pipeline_config=self.training_pipeline_config
             )
             model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,
                     data_transformation_artifact=data_transformation_artifact                         
@@ -112,7 +112,7 @@ class TrainingPipeline:
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
             self.sync_artifact_dir_to_s3()
-            self.sync_model_to_s3()
+            self.sync_saved_model_dir_to_s3()
             return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
